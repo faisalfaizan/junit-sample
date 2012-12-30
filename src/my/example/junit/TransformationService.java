001fc4266fe4a8cea20d5dab68a9a06bd3e76110
Faisal Faizan <faisalfaizan@gmail.com>
2012-12-30 23:38:41 +0530

    added makeChunk

--- src/my/example/junit/TransformationService.java
+++ src/my/example/junit/TransformationService.java
@@ -2,6 +2,8 @@ package my.example.junit;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TransformationService {
 
@@ -15,4 +17,18 @@ public class TransformationService {
 		sdf.applyPattern(pattern);
 		return sdf.format(date);
 	}
+	
+	public List<List<Object>> makeChunks(List<Object> list, int partitionSize){
+		List<List<Object>> resultList = new LinkedList<List<Object>>();
+		int size = list.size();
+		
+		for(int i = 0; i < size; i += partitionSize){
+			int lastIndex = i + partitionSize;
+			if(lastIndex > size)
+				lastIndex = size;
+			resultList.add(list.subList(i, lastIndex));
+		}
+		return resultList;
+	}
 }
+
--- test/my/example/junit/TransformationServiceTest.java
+++ test/my/example/junit/TransformationServiceTest.java
@@ -1,11 +1,14 @@
 package my.example.junit;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -35,19 +38,19 @@ public class TransformationServiceTest {
 	}
 
 	@Test
-	public void testTransform() {
+	public void transformStringToNumber() {
 		String str = "100";
 		int value = transformationService.transform(str);
 		assertSame(100, value);
 	}
 	
 	@Test(expected = NumberFormatException.class)
-	public void testTransformDataError() {
+	public void transformStringToNumberWithError() {
 		transformationService.transform("182k");
 	}
 	
 	@Test
-	public void testTransformDateToyyyyddMM() {
+	public void transformDateToStringFormatyyyyddMM() {
 		Calendar cal = GregorianCalendar.getInstance();
 		Date date = cal.getTime();
 		int day = cal.get(Calendar.DAY_OF_MONTH);
@@ -55,7 +58,89 @@ public class TransformationServiceTest {
 		int year = cal.get(Calendar.YEAR);
 		String actualValue = transformationService.transform(date);
 		String expectedValue = ""+ year + "-" + day + "-" + month; //yyyy-dd-MM
-	    assertEquals("Date same", expectedValue, actualValue);
+	    assertEquals("Same Date", expectedValue, actualValue);
 	}
 
+	@Test
+	public void transformListOfObjectsToSmallerChunks(){
+		List<List<Object>> expectedList = null;
+		List<Object> originalList = new ArrayList<Object>();
+		originalList.add("First");
+		originalList.add("Second");
+		originalList.add("Third");
+		originalList.add("Fourth");
+		originalList.add("Fifth");
+		
+		int partitionSize = 2;
+		expectedList = transformationService.makeChunks(originalList, partitionSize);
+		
+		assertSame("3 chunks made", expectedList.size(), 3);
+		assertSame(expectedList.get(0).size(), 2);
+		assertSame(expectedList.get(1).size(), 2);
+		assertSame(expectedList.get(2).size(), 1);
+		
+		assertArrayEquals(originalList.subList(0, 2).toArray(), expectedList.get(0).toArray());
+	}
+	
+	@Test
+	public void transformEmptyListToSmallerChunks(){
+		List<List<Object>> expectedList = null;
+		List<Object> originalList = new ArrayList<Object>();
+		
+		int partitionSize = 2;
+		expectedList = transformationService.makeChunks(originalList, partitionSize);
+		
+		assertSame("Empty", expectedList.size(), 0);
+	}
+	
+	@SuppressWarnings("unused")
+	@Test(expected=NullPointerException.class)
+	public void transformNullToSmallerChunks(){
+		List<List<Object>> expectedList = null;
+		List<Object> originalList = null;
+		
+		int partitionSize = 2;
+		expectedList = transformationService.makeChunks(originalList, partitionSize);
+	}
+	
+	@Test
+	public void transformEvenListToChunksOfOddSize(){
+		List<List<Object>> expectedList = null;
+		List<Object> originalList = new ArrayList<Object>();
+		originalList.add("First");
+		originalList.add("Second");
+		originalList.add("Third");
+		originalList.add("Fourth");
+		originalList.add("Fifth");
+		originalList.add("Sixth");
+		
+		int partitionSize = 3;
+		expectedList = transformationService.makeChunks(originalList, partitionSize);
+		
+		assertSame("2 chunks made", expectedList.size(), 2);
+		assertSame(expectedList.get(0).size(), 3);
+		assertSame(expectedList.get(1).size(), 3);
+			
+		assertArrayEquals(originalList.subList(0, 3).toArray(), expectedList.get(0).toArray());
+	}
+	
+	@Test
+	public void transformListToChunksOfSameSize(){
+		List<List<Object>> expectedList = null;
+		List<Object> originalList = new ArrayList<Object>();
+		originalList.add("First");
+		originalList.add("Second");
+		originalList.add("Third");
+		originalList.add("Fourth");
+		originalList.add("Fifth");
+		originalList.add("Sixth");
+		
+		int partitionSize = 6;
+		expectedList = transformationService.makeChunks(originalList, partitionSize);
+		
+		assertSame("Single chunk made", expectedList.size(), 1);
+		assertSame(expectedList.get(0).size(), 6);
+			
+		assertArrayEquals(originalList.subList(0, 6).toArray(), expectedList.get(0).toArray());
+	}
 }
